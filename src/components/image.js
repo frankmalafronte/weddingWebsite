51e45abc0adb4acc514bdb882b65114e2b3f3871
Frank <frankamalafronte@gmail.com>
2020-10-14 19:37:46 -0400

    added melbibi

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "Melbibi.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -26,7 +26,7 @@ const Image = () => {
     }
   `)
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+  if (!data.placeholderImage.childImageSharp.fluid) {
     return <div>Picture not found</div>
   }
 
