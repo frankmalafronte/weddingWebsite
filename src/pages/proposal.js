ebfecc35aa9b70200d94a373be1fba6a17aef457
Frank <frankamalafronte@gmail.com>
2020-10-18 19:50:21 -0400

    finished proposal gallary

--- src/pages/proposal.js
+++ src/pages/proposal.js
@@ -18,7 +18,14 @@ export default function Proposal() {
         }
         }
       }
-        melbibi: file(relativePath: { eq: "melbibi.jpg" }) {
+    smooch: file(relativePath: { eq: "proposal/smooch.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth:300 maxHeight:300 fit:FILL) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+        }
+      }
+        handsClasp: file(relativePath: { eq: "proposal/handsClasp.jpg" }) {
           childImageSharp {
             fluid(maxWidth:300 maxHeight:300 fit:FILL) {
               ...GatsbyImageSharpFluid_withWebp
@@ -32,13 +39,97 @@ export default function Proposal() {
               }
               }
             }
-            twoFranks: file(relativePath: { eq: "twoFranks.jpg" }) {
+            ahh: file(relativePath: { eq: "proposal/ah!!!.jpg" }) {
               childImageSharp {
                 fluid(maxWidth:300 maxHeight:300 fit:COVER) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
                 }
               }
+              poloroids: file(relativePath: { eq: "proposal/poloroids.jpg" }) {
+                childImageSharp {
+                  fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                  }
+                }
+                handsSign: file(relativePath: { eq: "proposal/handsSign.jpg" }) {
+                  childImageSharp {
+                    fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                    }
+                  }
+                  cuties: file(relativePath: { eq: "cuties.jpg" }) {
+                    childImageSharp {
+                      fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                      }
+                    }
+                    sign: file(relativePath: { eq: "proposal/sign.jpg" }) {
+                      childImageSharp {
+                        fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                          ...GatsbyImageSharpFluid_withWebp
+                        }
+                        }
+                      }
+                        coolGuy: file(relativePath: { eq: "proposal/coolGuy.jpg" }) {
+                          childImageSharp {
+                            fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                              ...GatsbyImageSharpFluid_withWebp
+                            }
+                            }
+                          }
+                          blush: file(relativePath: { eq: "proposal/blush.jpg" }) {
+                            childImageSharp {
+                              fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                ...GatsbyImageSharpFluid_withWebp
+                              }
+                              }
+                            }
+                            happyBecca: file(relativePath: { eq: "proposal/happyBecca.jpg" }) {
+                              childImageSharp {
+                                fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                  ...GatsbyImageSharpFluid_withWebp
+                                }
+                                }
+                              }
+                              beccaSmiles: file(relativePath: { eq: "proposal/beccaSmiles.jpg" }) {
+                                childImageSharp {
+                                  fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                    ...GatsbyImageSharpFluid_withWebp
+                                  }
+                                  }
+                                }
+                                waterFeet: file(relativePath: { eq: "proposal/waterFeet.jpg" }) {
+                                  childImageSharp {
+                                    fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                      ...GatsbyImageSharpFluid_withWebp
+                                    }
+                                    }
+                                  }
+                                  shoulder: file(relativePath: { eq: "proposal/shoulder.jpg" }) {
+                                    childImageSharp {
+                                      fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                        ...GatsbyImageSharpFluid_withWebp
+                                      }
+                                      }
+                                    }
+                                    nature: file(relativePath: { eq: "proposal/nature.jpg" }) {
+                                      childImageSharp {
+                                        fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                          ...GatsbyImageSharpFluid_withWebp
+                                        }
+                                        }
+                                      }
+                                      scenery: file(relativePath: { eq: "proposal/scenery.jpg" }) {
+                                        childImageSharp {
+                                          fluid(maxWidth:300 maxHeight:300 fit:COVER) {
+                                            ...GatsbyImageSharpFluid_withWebp
+                                          }
+                                          }
+                                        }
     }`)
 
 
@@ -48,30 +139,28 @@ return (
 <div>Hello Proposal</div>
 <div className ={styles.row}>
 <div id ="1" className={styles.column}>
-  {/* <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nunc ac vestibulum rutrum. In et eros euismod, lobortis diam dictum, rutrum ipsum. Maecenas sed molestie augue. Proin placerat tellus non lorem porttitor, quis pulvinar lorem tincidunt. Aliquam vel turpis sit amet ligula egestas efficitur et eu leo. Nullam euismod metus ut odio blandit, ac posuere risus auctor. Morbi ac nibh eros. Mauris tincidunt nunc quis velit ultricies aliquam. Nunc vitae libero neque. Quisque lobortis iaculis condimentum. Suspendisse commodo lobortis est, vitae finibus urna porta a. Aliquam eu elit efficitur sapien consequat sollicitudin. Morbi viverra erat non semper fermentum. Proin nisl lacus, malesuada vitae mauris a, iaculis auctor est. Nam tempor erat quis congue tristique.</div> */}
-  {/* <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nunc ac vestibulum rutrum. In et eros euismod, lobortis diam dictum, rutrum ipsum. Maecenas sed molestie augue. Proin placerat tellus non lorem porttitor, quis pulvinar lorem tincidunt. Aliquam vel turpis sit amet ligula egestas efficitur et eu leo. Nullam euismod metus ut odio blandit, ac posuere risus auctor. Morbi ac nibh eros. Mauris tincidunt nunc quis velit ultricies aliquam. Nunc vitae libero neque. Quisque lobortis iaculis condimentum. Suspendisse commodo lobortis est, vitae finibus urna porta a. Aliquam eu elit efficitur sapien consequat sollicitudin. Morbi viverra erat non semper fermentum. Proin nisl lacus, malesuada vitae mauris a, iaculis auctor est. Nam tempor erat quis congue tristique.</div> */}
-  <Img fluid = {data.usAndCats.childImageSharp.fluid} />
-  <Img fluid = {data.twoFranks.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
+  <Img fluid = {data.smooch.childImageSharp.fluid} />
+  <Img fluid = {data.poloroids.childImageSharp.fluid} />
+  <Img fluid = {data.sign.childImageSharp.fluid} />
+  <Img fluid = {data.cuties.childImageSharp.fluid} />
   </div>
   <div id="2" className={styles.column}>
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
+  <Img fluid = {data.handsClasp.childImageSharp.fluid} />
+  <Img fluid = {data.handsSign.childImageSharp.fluid} />
+  <Img fluid = {data.sign.childImageSharp.fluid} />
+  <Img fluid = {data.ahh.childImageSharp.fluid} />
   </div>
   <div id="3" className={styles.column}>
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
+  <Img fluid = {data.coolGuy.childImageSharp.fluid} />
+  <Img fluid = {data.happyBecca.childImageSharp.fluid} />
+  <Img fluid = {data.blush.childImageSharp.fluid} />
+  <Img fluid = {data.beccaSmiles.childImageSharp.fluid} />
   </div>
   <div id="4" className={styles.column}>
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
-  <Img fluid = {data.drBoom.childImageSharp.fluid} />
+  <Img fluid = {data.nature.childImageSharp.fluid} />
+  <Img fluid = {data.waterFeet.childImageSharp.fluid} />
+  <Img fluid = {data.shoulder.childImageSharp.fluid} />
+  <Img fluid = {data.scenery.childImageSharp.fluid} />
   </div>
  </div>
  </Links>
