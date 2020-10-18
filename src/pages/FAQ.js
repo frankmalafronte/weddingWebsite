ea1120845d489fc474ebdd9ef3885a1fe27ab283
Becca Brenner <beccabrenner@macbook-pro.myfiosgateway.com>
2020-10-18 14:09:10 -0400

    why zoom?

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -9,13 +9,13 @@ import FAQComponent from '../components/FAQ'
 export default function FAQ() {
   const [faqs, setFaqs] = useState([
     {
-    question: 'Why a Zoom Wedding?',
+    question: 'Why did you choose to do a Zoom Wedding?',
     answer: `If there’s one thing that 2020 has taught us, it’s that life is full of the unexpected — twists and turns, ups and downs. The past many months have taught us a lot about ourselves and each other, and now more than ever, we are ready to start our lives together.
     We want to be able to celebrate with all of our loved ones, near and far. With no clear end in sight for the pandemic, we are not confident that we’ll be able to safely gather and celebrate the way we want to within the next year.
     We hope to have a massive, lively, joyful in-person celebration sometime in 2022, but we just couldn’t wait to tie the knot!
     SO, we were inspired by the creativity of other friends and family to instead consider an hosting a Zoom wedding. We chose a date that holds special significance for us: December 31, 2020, which will mark five years since we first met at a New Year’s Eve party.
     We hope you’ll be able to join us before the ball drops (or doesn’t — who knows?) and close out 2020 on a happy note, despite all the challenges it has brought us.
-    To RSVP to our Zoom wedding and receive an invitation, click here.`,
+    To RSVP to our Zoom wedding and receive an invitation, click here!`,
     open:false
   },
   {
