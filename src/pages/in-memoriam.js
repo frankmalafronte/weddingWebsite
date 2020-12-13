5b45b4c96af50892f02d8790044961b105dadac3
Frank <frankamalafronte@gmail.com>
2020-12-13 18:10:47 -0500

    becca fix

--- src/pages/in-memoriam.js
+++ src/pages/in-memoriam.js
@@ -54,7 +54,7 @@ export default function InMemoriam() {
                     }
                   garyAppleby: file(relativePath: { eq: "inMemoriam/gary-appleby-2.jpg" }) {
                     childImageSharp {
-                      fluid(maxWidth:1200 maxHeight:1200 fit:CONTAIN) {
+                      fluid(maxWidth:1200 maxHeight:1200 fit:COVER cropFocus:NORTH) {
                         ...GatsbyImageSharpFluid_withWebp
                       }
                       }
